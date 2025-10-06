61dfefc0abe944a5fbf05c6cebeaa205bee42568
gnaiG19804 <tvgiang19082004@gmail.com>
2025-10-06 15:31:04 +0700

    feat(frontend): hoàn thiện chức năng đăng nhập

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage"; 
-import AuthCallback from "./pages/AuthCallback"; 
-
 
 export default function App() {
   return (
       <BrowserRouter>
         <Routes>
-           <Route path="/" element={<LoginPage/>} />
-          <Route path="/auth/callback" element={<AuthCallback/>} />
+           <Route path="/login" element={<LoginPage/>} />
         </Routes>
       </BrowserRouter>
   );
