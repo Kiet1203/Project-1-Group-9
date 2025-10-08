a1f586375ff73f4fa6c57653b4f08d6e173aae3b
gnaiG19804 <tvgiang19082004@gmail.com>
2025-10-08 14:21:18 +0700

    feat(ui): header trang chủ

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Header from '../components/home/Header';
+const Homepage = () => {
+  return (
+      <Header />
+
+  );
+};
+
+export default Homepage;
